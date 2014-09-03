13f64c2ab1b58b6f77a29ca899f28c0831ee5de9
Andrew Watkins <andrew.watkinz@gmail.com>
2014-09-03 14:07:52 -0600

    adds instructions on where to go in root and adds run button

--- lib/app.rb
+++ lib/app.rb
@@ -7,6 +7,10 @@ Haml::Options.defaults[:ugly] = true
 class MiniatureOctoIronman < Sinatra::Base
   set :markdown, layout_engine: :haml, layout: :layout
 
+  get '/' do
+    "please go to localhost:somenumber/lesson1"
+  end
+
   get '/lesson1' do
     markdown :lesson1
   end
