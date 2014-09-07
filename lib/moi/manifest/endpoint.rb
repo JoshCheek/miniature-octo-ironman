5b3c12496ac21c2e8f83524b94771ebedf5f9f47
Josh Cheek <josh.cheek@gmail.com>
2014-09-07 17:50:30 -0600

    Add owner and path to an endpoint

--- lib/moi/manifest/endpoint.rb
+++ lib/moi/manifest/endpoint.rb
@@ -1,16 +1,18 @@
 module Moi
   class Manifest
     class Endpoint
-      attr_accessor :repo, :ref, :file, :localpath
+      attr_accessor :repo, :ref, :file, :owner, :path, :localpath
 
       def initialize(attributes)
         missing = []
         init_attribute :repo,      attributes, missing
         init_attribute :ref,       attributes, missing
         init_attribute :file,      attributes, missing
+        init_attribute :owner,     attributes, missing
+        init_attribute :path,      attributes, missing
         init_attribute :localpath, attributes
 
-        extra = attributes.keys - [:repo, :ref, :file, :localpath]
+        extra = attributes.keys - [:repo, :ref, :file, :owner, :path, :localpath]
         raise ArgumentError, "Extra attributes: #{extra.inspect}"     if extra.any?
         raise ArgumentError, "Missing attributes: #{missing.inspect}" if missing.any?
       end
--- spec/manifest/manifest_spec.rb
+++ spec/manifest/manifest_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe 'Moi::Manifest' do
     Moi::Manifest::Endpoint.new attributes
   end
 
+  # raises error if multiple repos have same owner and endpoint
+
   it 'receives an array of hashes or endpoints that it converts to endpoints' do
-    endpoints = [ {repo: 'repo1', ref: 'ref1', file: 'file1'},
-                  Moi::Manifest::Endpoint.new(repo: 'repo2', ref: 'ref2', file: 'file2')]
+    endpoints = [ {repo: 'repo1', ref: 'ref1', file: 'file1', owner: 'owner1', path: 'path1'},
+                  Moi::Manifest::Endpoint.new(repo: 'repo2', ref: 'ref2', file: 'file2', owner: 'owner1', path: 'path2')]
     manifest = Moi::Manifest.new endpoints
     expect(manifest.size).to eq 2
     expect(manifest[0].repo).to eq 'repo1'
@@ -15,23 +17,27 @@ RSpec.describe 'Moi::Manifest' do
   end
 
   it 'has all that Enumerable shit' do
-    endpoints = [ {repo: 'repo1', ref: 'ref1', file: 'file1'},
-                  Moi::Manifest::Endpoint.new(repo: 'repo2', ref: 'ref2', file: 'file2')]
+    endpoints = [ {repo: 'repo1', ref: 'ref1', file: 'file1', owner: 'owner1', path: 'path1'},
+                  Moi::Manifest::Endpoint.new(repo: 'repo2', ref: 'ref2', file: 'file2', owner: 'owner1', path: 'path2')]
     manifest = Moi::Manifest.new endpoints
     expect(manifest.map(&:repo)).to eq %w[repo1 repo2]
   end
 
   describe 'Moi::Manifest::Endpoint' do
-    let(:repo)             { 'somerepo' }
-    let(:ref)              { 'someref'  }
-    let(:file)             { 'somefile' }
-    let(:valid_attributes) {{repo: repo, ref: ref, file: file}}
+    let(:repo)             { 'somerepo'   }
+    let(:ref)              { 'someref'    }
+    let(:file)             { 'somefile'   }
+    let(:owner)            { 'someowner'  }
+    let(:path)             { 'somepath'   }
+    let(:valid_attributes) {{repo: repo, ref: ref, file: file, owner: owner, path: path}}
 
-    it 'has an endpoint, git repo, ref, and file' do
+    it 'has an git repo, ref, file, owner, and path' do
       manifest = endpoint_for valid_attributes
-      expect(manifest.repo).to eq repo
-      expect(manifest.ref).to  eq ref
-      expect(manifest.file).to eq file
+      expect(manifest.repo ).to eq repo
+      expect(manifest.ref  ).to eq ref
+      expect(manifest.file ).to eq file
+      expect(manifest.owner).to eq owner
+      expect(manifest.path ).to eq path
     end
 
     it 'optionally takes a localpath' do
@@ -43,11 +49,16 @@ RSpec.describe 'Moi::Manifest' do
     end
 
     it 'raises an error if any attributes are missing' do
-      expect { endpoint_for repo: repo, ref: ref, file: file}.to_not raise_error
-      expect { endpoint_for             ref: ref, file: file}.to raise_error ArgumentError, /Missing attributes: \[:repo\]/
-      expect { endpoint_for repo: repo,           file: file}.to raise_error ArgumentError, /Missing attributes: \[:ref\]/
-      expect { endpoint_for repo: repo, ref: ref            }.to raise_error ArgumentError, /Missing attributes: \[:file\]/
-      expect { endpoint_for Hash.new                        }.to raise_error ArgumentError, /Missing attributes: \[:repo, :ref, :file\]/
+      endpoint_without = lambda do |*keys|
+        invalid_attributs = valid_attributes.reject { |k, v| keys.any? { |key| key == k } }
+        endpoint_for invalid_attributs
+      end
+      expect { valid_attributes }.to_not raise_error
+      expect { endpoint_without[:repo ]}.to raise_error ArgumentError, /Missing attributes: \[:repo\]/
+      expect { endpoint_without[:ref  ]}.to raise_error ArgumentError, /Missing attributes: \[:ref\]/
+      expect { endpoint_without[:file ]}.to raise_error ArgumentError, /Missing attributes: \[:file\]/
+      expect { endpoint_without[:owner]}.to raise_error ArgumentError, /Missing attributes: \[:owner\]/
+      expect { endpoint_without[:path ]}.to raise_error ArgumentError, /Missing attributes: \[:path\]/
     end
 
     it 'raises an error if extra keys are provided' do
