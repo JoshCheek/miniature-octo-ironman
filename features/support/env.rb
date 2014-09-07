27c78869fcc951e56d6c39c896fc3a31f4351d3b
Josh Cheek <josh.cheek@gmail.com>
2014-09-06 20:51:18 -0600

    Extract step defs into their own file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
+# load our helpers
 $LOAD_PATH.unshift File.expand_path('../..', __FILE__)
 require 'support/stupid_stub_lib'
 require 'support/our_helpers'
-
 
 # Start our server
 OurHelpers.start_server
@@ -11,7 +11,7 @@ at_exit { OurHelpers.stop_server }
 require 'webmock'
 WebMock.disable_net_connect!
 
-# We'll stick our stubs here
+# Custom stub lib since RSpec mocks apparently doesn't work
 CukeStubs = StupidStubLib.new
 
 # Things to add to the Cucumber world
@@ -30,37 +30,4 @@ end
 # Remove stubs
 After do
   CukeStubs.unstub self
-end
-
-Given 'eval.in will serve "$url" as:' do |url, json|
-  parsed_json = JSON.parse(json).merge('url' => url)
-  result      = EvalIn.build_result(parsed_json)
-  CukeStubs.stub self, EvalIn, :call, result
-end
-
-Given 'I have a document "$name":' do |name, body|
-  File.write path_to_view(name), body
-end
-
-When 'I visit "$path"' do |path|
-  require 'redcarpet'
-  internet.visit "http://localhost:1235#{path}"
-end
-
-Then 'my page has "$content" on it' do |content|
-  expect(internet.body).to include content
-end
-
-Then 'my page has an editor with "$content"' do |content|
-  internet.within editor_class do
-    expect(internet).to have_content(content)
-  end
-end
-
-When 'I submit the code in the editor' do
-  internet.click_on 'Run'
-end
-
-Then 'I see an output box with "$content" in it' do |content|
-  expect(internet).to have_css displayed_result_class, text: content
 end
---
+++ features/support/step_definitions.rb
@@ -0,0 +1,32 @@
+Given 'eval.in will serve "$url" as:' do |url, json|
+  parsed_json = JSON.parse(json).merge('url' => url)
+  result      = EvalIn.build_result(parsed_json)
+  CukeStubs.stub self, EvalIn, :call, result
+end
+
+Given 'I have a document "$name":' do |name, body|
+  File.write path_to_view(name), body
+end
+
+When 'I visit "$path"' do |path|
+  require 'redcarpet' # TODO: Wat is this?
+  internet.visit "http://localhost:1235#{path}"
+end
+
+Then 'my page has "$content" on it' do |content|
+  expect(internet.body).to include content
+end
+
+Then 'my page has an editor with "$content"' do |content|
+  internet.within editor_class do
+    expect(internet).to have_content(content)
+  end
+end
+
+When 'I submit the code in the editor' do
+  internet.click_on 'Run'
+end
+
+Then 'I see an output box with "$content" in it' do |content|
+  expect(internet).to have_css displayed_result_class, text: content
+end
