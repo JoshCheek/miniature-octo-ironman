747d608f7a902177b8248e49a0518f08beefeda6
Josh Cheek <josh.cheek@gmail.com>
2014-09-07 10:22:52 -0600

    Refactor ruby_dependency into an instance

--- lib/moi/ruby_dependency.rb
+++ lib/moi/ruby_dependency.rb
@@ -2,27 +2,48 @@ require 'rubygems/version'
 require 'rubygems/requirement'
 
 class Moi
-  module RubyDependency
+  class RubyDependency
+
     # @example
     #   Moi::RubyDependency.call requirement: '~> 2.1.0',
     #                            default:     '2.1.2,
     #                            current:     RUBY_VERSION
     def self.call(params)
-      requirement = Gem::Requirement.new fetch(params, :requirement)
-      default     = Gem::Version.new     fetch(params, :default)
-      current     = Gem::Version.new     fetch(params, :current)
+      new(params).call
+    end
 
-      requirement.satisfied_by?(default) or
-        raise Gem::DependencyError, "Default (#{default}) does not satisfy requirement (#{requirement})"
+    def initialize(params)
+      @params = params
+    end
 
-      (requirement.satisfied_by?(current) ? current : default).version
+    def call
+      default_must_satisfy_requirement!
+      requirement.satisfied_by?(current) ? current.version
+                                         : default.version
     end
 
     private
 
-    def self.fetch(params, key)
-      params[key] or
-        raise ArgumentError, "#{params.inspect} does not have key #{key.inspect}"
+    def requirement
+      @requirement ||= Gem::Requirement.new fetch(:requirement)
+    end
+
+    def default
+      @default ||= Gem::Version.new fetch(:default)
+    end
+
+    def current
+      @current ||= Gem::Version.new fetch(:current)
+    end
+
+    def fetch(key)
+      return @params[key] if @params[key]
+      raise ArgumentError, "#{@params.inspect} does not have key #{key.inspect}"
+    end
+
+    def default_must_satisfy_requirement!
+      requirement.satisfied_by?(default) or
+        raise Gem::DependencyError, "Default (#{default}) does not satisfy requirement (#{requirement})"
     end
   end
 end
