5ee225e9bc7452ac564183d82fce8ada99ea3381
Josh Cheek <josh.cheek@gmail.com>
2014-09-07 10:58:37 -0600

    Main page redirects to lesson1 instead of telling you to navigate there

--- lib/app.rb
+++ lib/app.rb
@@ -9,7 +9,7 @@ class MiniatureOctoIronman < Sinatra::Base
   set :markdown, layout_engine: :haml, layout: :layout
 
   get '/' do
-    "please go to localhost:somenumber/lesson1"
+    redirect '/lesson1'
   end
 
   get '/lesson1' do
