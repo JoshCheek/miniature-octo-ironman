6b4ce1261d47ddea18b07c581de6b10f0bdaf712
Josh Cheek <josh.cheek@gmail.com>
2014-09-07 19:01:12 -0600

    Add datadir to endpoint, it generates localpath and fullpath

--- lib/moi/manifest/endpoint.rb
+++ lib/moi/manifest/endpoint.rb
@@ -1,28 +1,43 @@
+require 'pathname'
+
 module Moi
   class Manifest
     class Endpoint
-      attr_accessor :repo, :ref, :file, :owner, :webpath, :localpath
+      attr_accessor :repo, :ref, :file, :owner, :webpath, :datadir
 
       def initialize(attributes)
-        self.extra   = attributes.keys - [:repo, :ref, :file, :owner, :webpath, :localpath]
+        self.extra   = attributes.keys - [:repo, :ref, :file, :owner, :webpath, :localpath, :datadir]
         self.missing = []
         init_attribute :repo,      attributes, missing
         init_attribute :ref,       attributes, missing
         init_attribute :file,      attributes, missing
         init_attribute :owner,     attributes, missing
         init_attribute :webpath,   attributes, missing
+        init_attribute :datadir,   attributes, missing
         init_attribute :localpath, attributes
+      end
+
+      attr_writer :localpath
+
+      def fullpath
+        datadir && File.join(datadir, localpath)
+      end
+
+      def localpath
+        @localpath || File.join(owner, reponame)
       end
 
       def valid?
         !error
       end
 
-      def error
+      def error # should prob do this dynamically
         if missing.any?
           "Missing attributes: #{missing.inspect}"
         elsif extra.any?
           "Extra attributes: #{extra.inspect}"
+        elsif localpath && localpath.start_with?("/")
+          "localpath should not be absolute, but it is #{localpath.inspect}"
         end
       end
 
@@ -36,6 +51,10 @@ module Moi
         elsif missing
           missing << name
         end
+      end
+
+      def reponame
+        Pathname.new(repo).basename.sub_ext("")
       end
     end
   end
--- spec/manifest/manifest_spec.rb
+++ spec/manifest/manifest_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'Moi::Manifest' do
 
   # TODO invalid if multiple repos have same owner and endpoint
 
+  it 'allows access to the endpoint via the owner/webpath pair'
+
   it 'receives an array of hashes or endpoints that it converts to endpoints' do
     endpoints = [ {repo: 'repo1', ref: 'ref1', file: 'file1', owner: 'owner1', webpath: 'webpath1'},
                   Moi::Manifest::Endpoint.new(repo: 'repo2', ref: 'ref2', file: 'file2', owner: 'owner1', webpath: 'webpath2')]
@@ -24,33 +26,58 @@ RSpec.describe 'Moi::Manifest' do
   end
 
   describe 'Moi::Manifest::Endpoint' do
-    let(:repo)             { 'somerepo'    }
-    let(:ref)              { 'someref'     }
-    let(:file)             { 'somefile'    }
-    let(:owner)            { 'someowner'   }
-    let(:webpath)          { 'somewebpath' }
-    let(:valid_attributes) {{repo: repo, ref: ref, file: file, owner: owner, webpath: webpath}}
+    let(:reponame)         { 'miniature-octo-ironman'                       }
 
-    it 'has an git repo, ref, file, owner, and webpath' do
-      manifest = endpoint_for valid_attributes
-      expect(manifest.repo   ).to eq repo
-      expect(manifest.ref    ).to eq ref
-      expect(manifest.file   ).to eq file
-      expect(manifest.owner  ).to eq owner
-      expect(manifest.webpath).to eq webpath
+    let(:repo)             { "https://github.com/JoshCheek/#{reponame}.git" }
+    let(:ref)              { 'someref'                                      }
+    let(:file)             { 'somefile'                                     }
+    let(:owner)            { 'someowner'                                    }
+    let(:webpath)          { 'somewebpath'                                  }
+    let(:datadir)          { 'somedatadir'                                  }
+    let(:valid_attributes) {{repo: repo, ref: ref, file: file, owner: owner, webpath: webpath, datadir: datadir}}
+    let(:endpoint)         { endpoint_for valid_attributes }
+
+    it 'has a git repo, ref, file, owner, datadir, and webpath' do
+      expect(endpoint.repo   ).to eq repo
+      expect(endpoint.ref    ).to eq ref
+      expect(endpoint.file   ).to eq file
+      expect(endpoint.owner  ).to eq owner
+      expect(endpoint.webpath).to eq webpath
+      expect(endpoint.datadir).to eq datadir
     end
 
-    it 'optionally takes a localpath' do
-      endpoint = endpoint_for(valid_attributes)
-      expect(endpoint.localpath).to eq nil
+    describe '#localpath/#fullpath' do
+      context 'when there is not a localpath' do
+        it 'chooses a localpath of the_owner/reponame, within the datadir' do
+          expect(endpoint.fullpath).to  eq "#{datadir}/#{owner}/#{reponame}"
+          expect(endpoint.localpath).to eq "#{owner}/#{reponame}"
+        end
+      end
 
-      endpoint = endpoint_for(valid_attributes.merge localpath: 'somepath')
-      expect(endpoint.localpath).to eq 'somepath'
+      context 'when there is a localpath' do
+        it 'respects the localpath, but still assumes it is within the datadir' do
+          endpoint.localpath = "a/b/c"
+          expect(endpoint.fullpath).to  eq "#{datadir}/a/b/c"
+          expect(endpoint.localpath).to eq "a/b/c"
+        end
+      end
+
+      context 'when there is not a datadir' do
+        before { endpoint.datadir = nil }
+
+        it 'chooses a localpath of the_owner/reponame' do
+          expect(endpoint.localpath).to eq "#{owner}/#{reponame}"
+        end
+
+        it 'has no fullpath' do
+          expect(endpoint.fullpath).to eq nil
+        end
+      end
     end
 
     context 'validation/errors' do
       def each_invalid
-        [:repo, :ref, :file, :owner, :webpath].each do |attribute|
+        [:repo, :ref, :file, :owner, :webpath, :datadir].each do |attribute|
           invalid_attributs = valid_attributes.reject { |k, v| k == attribute }
           yield attribute, endpoint_for(invalid_attributs)
         end
@@ -79,6 +106,12 @@ RSpec.describe 'Moi::Manifest' do
 
       it 'has an error string that explains what keys are extra' do
         expect(endpoint_for(valid_attributes.merge extra: 'val').error).to eq "Extra attributes: [:extra]"
+      end
+
+      it 'is invalid with error if localpath is absolute' do
+        endpoint = endpoint_for valid_attributes.merge(localpath: "/a")
+        expect(endpoint).to_not be_valid
+        expect(endpoint.error).to match /absolute/
       end
     end
   end
