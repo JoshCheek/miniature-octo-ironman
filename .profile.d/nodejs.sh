2c556efb2a7dd89ed3575bb3f7d67ddb5230a675
Josh Cheek <josh.cheek@gmail.com>
2014-10-17 09:37:33 -0600

    Remove old Heroku stuff that never worked

--- .profile.d/nodejs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Faking out https://github.com/vizify/heroku-buildpack-multi/blob/04c4a692e464e5e52bd193c8e6a3d68a004d3aa1/bin/compile#L75
-# For a rant about this, read https://github.com/JoshCheek/heroku-buildpack-for-cmake-and-pkg-config/blob/master/bin/release
